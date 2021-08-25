b6b9c60adeadc224bc0f24ad87de77b9e27882c8
therandomfool <mddenver@protonmail.com>
2021-08-25 00:54:59 -0600

    text color in timeline background color lighten

--- src/styles/GlobalComponents/index.js
+++ src/styles/GlobalComponents/index.js
@@ -60,7 +60,7 @@ export const SectionText = styled.p`
   line-height: 40px;
   font-weight: 300;
   padding-bottom: 3.6rem;
-  color: rgba(255, 255, 255, 0.5);
+  color: rgba(255, 255, 255, 0.79);
 
   @media ${(props) => props.theme.breakpoints.md} {
     max-width: 670px;
